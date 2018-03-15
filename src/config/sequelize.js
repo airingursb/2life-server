4f41b42f0652122b52f8450efdee28748ed8644c
Airing <gx-deng@163.com>
2018-03-15 21:56:07 +0800

    feat: 修改数据格式为utf8mb4，以支持emoji

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -3,9 +3,20 @@ import Sequelize from 'sequelize'
 import {SQL_USER, SQL_PASSWORD} from './index'
 
 export const dbConnect = () => {
-  return new Sequelize(
-    'twolife',
-    SQL_USER,
-    SQL_PASSWORD,
-    {'dialect': 'mysql', host: 'localhost', port: 3306})
+  return new Sequelize('twolife', SQL_USER, SQL_PASSWORD,
+    {
+      'dialect': 'mysql',
+      'dialectOptions': {
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_unicode_ci',
+        supportBigNumbers: true,
+        bigNumberStrings: true
+      },
+      'host': 'localhost',
+      'port': 3306,
+      'define': {
+        'underscored': true // 字段以下划线（_）来分割（默认是驼峰命名风格）
+      }
+    }
+  )
 }

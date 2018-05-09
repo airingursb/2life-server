56986c0d481e4ff774797fbfb92a7f133d74296b
Airing <gx-deng@163.com>
2018-05-09 14:33:19 +0800

    feat(mode): add modes/show

--- src/routes/modes.js
+++ src/routes/modes.js
@@ -1,9 +1,30 @@
 import express from 'express'
 
+import { Note } from '../models'
+
+import {
+  MESSAGE,
+  validate
+} from '../config'
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  return res.json({title: 'Express'})
+router.get('/show', (req, res) => {
+
+  const { uid, token, timestamp } = req.query
+  validate(res, true, uid, timestamp, token)
+
+  const response = async () => {
+    const notes = await Note.findAll({ where: { user_id: uid } })
+
+    const data = await notes.map(n => {
+      const d = {}
+      d[n.dataValues.date] = n.dataValues.mode
+      return d
+    })
+    return res.json({ ...MESSAGE.OK, data })
+  }
+  response()
 })
 
 module.exports = router

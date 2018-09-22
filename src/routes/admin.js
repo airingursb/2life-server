bfdf41b2b73bdfaad2cd1c8b7567022aa53391cb
Airing <gx-deng@163.com>
2018-09-22 11:07:49 +0800

    feat(admin): 提供用户操作相关接口

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -8,6 +8,8 @@ import {
   JiGuangPush,
 } from '../config'
 
+import { User } from '../models'
+
 const router = express.Router()
 
 // 反馈回复接口：推送+消息
@@ -39,5 +41,78 @@ router.post('/reply', (req, res) => {
 
   response()
 })
+
+
+// 用户列表接口
+router.get('/user/list', (req, res) => {
+
+  const { account, password, page, limit } = req.body
+
+  validate(res, false, uid, account, password, page, limit)
+
+  const response = async () => {
+
+    if (page <= 0 || limit < 1) {
+      return res.jsonp(MESSAGE.REQUEST_ERROR)
+    }
+
+    if (account !== ADMIN_USER || password !== ADMIN_PASSWORD) {
+      return res.jsonp(MESSAGE.PASSWORD_ERROR)
+    }
+
+    const users = await User.findAll({ offset: page * limit, limit })
+    return res.jsonp({ ...MESSAGE.OK, data: users })
+  }
+
+  response()
+})
+
+// 修改匹配次数接口
+router.post('/user/last_times', (req, res) => {
+
+  const { account, password, uid, last_times } = req.body
+
+  validate(res, false, uid, account, password, last_times)
+
+  const response = async () => {
+
+    if (last_times < 0) {
+      return res.jsonp(MESSAGE.REQUEST_ERROR)
+    }
+
+    if (account !== ADMIN_USER || password !== ADMIN_PASSWORD) {
+      return res.jsonp(MESSAGE.PASSWORD_ERROR)
+    }
+
+    await User.update({ last_times }, { where: { id: uid } })
+
+    return res.jsonp(MESSAGE.OK)
+  }
+
+  response()
+})
+
+
+// 修改会员时间接口
+router.post('/user/vip_expires', (req, res) => {
+
+  const { account, password, uid, vip_expires, vip } = req.body
+
+  validate(res, false, uid, account, password, vip_expires, vip)
+
+
+  const response = async () => {
+    if (account !== ADMIN_USER || password !== ADMIN_PASSWORD) {
+      return res.jsonp(MESSAGE.PASSWORD_ERROR)
+    }
+
+    await User.update({ vip_expires, vip }, { id: uid })
+
+    return res.jsonp(MESSAGE.OK)
+  }
+
+  response()
+})
+
 
 module.exports = router
